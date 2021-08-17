fdd2932440bc7b6835ff4fe44bef8fecaff90c03
woongjichoi <71050595+woongjichoi@users.noreply.github.com>
2021-08-17 11:32:22 +0900

    Add files via upload

---
+++ chatbotdemo/app/src/main/java/org/techtown/chatbotdemo/HamburgerMenuPage.java
@@ -0,0 +1,14 @@
+package org.techtown.chatbotdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HamburgerMenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hamburger_menu_page);
+    }
+}
